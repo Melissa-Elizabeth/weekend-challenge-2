b77d82e316373fba44067143a357e1b49d64a996
Avatar <avatar@Avatars-MacBook-Pro.local>
2017-02-19 21:51:59 -0600

    no index points

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,63 +1,64 @@
   var arrayOfPeople = [];
+  var clickNumber = 0;
 
 $(document).ready(function(){
   console.log("");
-
-
-
   // make AJAX reqest
-
   $.ajax({
     url: '/data',
     method: 'GET',
     success: function(data) {
       // when the response returns
-      console.log('data from server: ', data.phirephiters);
+      // console.log('data from server: ', data.phirephiters);
       var phirephitersArray = data.phirephiters;
       buildPhirephitersArray(phirephitersArray);
-      console.log(arrayOfPeople);
+      // console.log(arrayOfPeople);
       loopingThroughPeople(arrayOfPeople);
-    }
-  });
+
+      $('#next').on('click', function() {
+        clickNumber++;
+        if(clickNumber > arrayOfPeople.length - 1) {
+          clickNumber = 0;
+        }
+        // console.log(clickNumber);
+        shiftingThroughPeople();
+      });
+
+
+        $('#previous').on('click', function() {
+          clickNumber--;
+          if(clickNumber < 0) {
+            clickNumber = arrayOfPeople.length - 1;
+          }
+          // console.log(clickNumber);
+          shiftingThroughPeople();
+      });
+
+  }
+
+
 });
+
+});
+
 
 function buildPhirephitersArray(array) {
   for (var i = 0; i < array.length; i++) {
   arrayOfPeople.push(array[i]);
   }
 }
-
-
-
-
-  //     $el = arrayOfPersonDivs[0];
-  //     $el.show();
-  //   } // end of success
-  // });
-
-
-//   $("phirephiters[i]").hide();
-//   nextDiv.show();
-// });
-//
-  function appendPersonToDOM(person) {
-    $('#dataContainer').append('<div class="person"</div>');
-    var $el = $('#dataContainer');
-    //  $el.append('<img src ="' + person.image + '" height = "200px" width = "200px"/>');
-    $el.append('<h1>' + person.name + '</h1>');
-    $el.append('<h2>' +  person.git_username + '</h2>');
-    $el.append('<h3>' + person.shoutout + '</h3>');
-    // arrayOfPersonDi.push($el);
-    // $el.hide();
-  }
- function loopingThroughPeople(array) {
-   for (var i = 0; i < array.length; i++) {
-     appendPersonToDOM(array[i]);
-   }
+function appendPersonToDOM(person) {
+  $('#dataContainer').append('<div class="person"</div>');
+  var $el = $('#dataContainer');
+  $el.append('<img src ="' + person.image + '" height = "300px" width = "300px"/>');
+  $el.append('<h1> Name: ' + person.name + '</h1>');
+  $el.append('<h2> Github Username: ' +  person.git_username + '</h2>');
+  $el.append('<h3> Shoutout: ' + person.shoutout + '</h3>');
+}
+function loopingThroughPeople(array) {
+ appendPersonToDOM(array[clickNumber]);
  }
-
-
-//
-//
-//   }
-// });
+function shiftingThroughPeople() {
+  $('#dataContainer').empty();
+  loopingThroughPeople(arrayOfPeople);
+}
